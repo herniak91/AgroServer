a20363cd0dfcccfbbe1cb7e2c4bb2dd91ee23253
Hernan <Hernan@Home>
2016-05-08 18:22:59 -0300

    algo mas

--- AgroServer/src/main/java/com/hwilliams/agroServer/controller/Consultas.java
+++ AgroServer/src/main/java/com/hwilliams/agroServer/controller/Consultas.java
@@ -1,8 +1,10 @@
 package com.hwilliams.agroServer.controller;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.jsoup.Jsoup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +40,16 @@ public class Consultas {
 	public Usuario getuser(@RequestParam(name="name",required=true)String name, @RequestParam(name="lastname",required=true)String lastname){
 		logger.debug("Se busco informacion del  usuario de nombre: [" + name + "] y apellido: [" + lastname + "]");
 		return usuarioService.getUserById("COTO", "youMammaIsSoFat...");
+	}
+	
+	@RequestMapping(value="/getClima")
+	public String getClimaHtml(){
+		try {
+			return Jsoup.connect("").get().select(".css_class").html();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "Unable to get or parse html";
+		}
 	}
 	
 }
