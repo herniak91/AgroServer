546d8df0b80203316d3312749defa1a3818c9192
Hernan <Hernan@Home>
2016-05-08 17:56:34 -0300

    recuperando trabajo

---
+++ AgroServer/src/main/java/com/hwilliams/agroServer/controller/Consultas.java
@@ -0,0 +1,43 @@
+package com.hwilliams.agroServer.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hwilliams.agroServer.model.Usuario;
+import com.hwilliams.agroServer.service.UsuarioService;
+
+@RestController
+public class Consultas {
+	Logger logger = LoggerFactory.getLogger(Consultas.class);
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@RequestMapping(value="/test")
+	public String test(){
+		return "Controllers are up and running.";
+	}
+	
+	@RequestMapping(value="/getGralInformation")
+	public Map<String, Object> getGralInformation(){
+		logger.debug("Pedido de informacion general");
+		Map<String, Object> datos= new HashMap<>();
+		datos.put("clima", "hace frio");
+		datos.put("dolar", new Double(14.57));
+		return datos;
+	}
+	
+	@RequestMapping(value="/getUser")
+	public Usuario getuser(@RequestParam(name="name",required=true)String name, @RequestParam(name="lastname",required=true)String lastname){
+		logger.debug("Se busco informacion del  usuario de nombre: [" + name + "] y apellido: [" + lastname + "]");
+		return usuarioService.getUserById("COTO", "youMammaIsSoFat...");
+	}
+	
+}
